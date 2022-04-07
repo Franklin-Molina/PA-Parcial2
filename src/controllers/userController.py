a219b7c2f90f1798a078bc29fe619e76463cdff1
Franklin-Molina <asdfglxl7@gmail.com>
2022-04-07 01:38:54 -0500

    'email-validate-all'

--- src/controllers/userController.py
+++ src/controllers/userController.py
@@ -1,4 +1,5 @@
 from pickle import FALSE
+from turtle import isvisible
 from flask import render_template, request, redirect, url_for, jsonify, flash, session
 from src import app
 from src.models.user import UsuarioModel
@@ -18,14 +19,7 @@ def before_request():
 
 @app.route("/usuario/create", methods =['POST','GET'])
 def createUser():
-     if request.method ==  'POST':      
-
-
-               
-
-                        
-               
-
+     if request.method ==  'POST':    
 
                 usuarioModel = UsuarioModel()
                 name=request.form.get('usuario')
@@ -54,10 +48,12 @@ def createUser():
                 if email == "":
                         flash("Correo Obligatorio")   
                         isValid=False    
-                if re.search('[@]',email):
+                if re.search("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",email):
                         valimail= True
+
                 if valimail == False:
-                        flash("Correo no valido (@)")                                  
+                        isValid=False  
+                        flash("Correo no valido")                                  
                
                 if password == "":
                         flash("Contraseña Obligatoria")  
